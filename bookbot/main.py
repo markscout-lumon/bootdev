deaec7ec0ddf4e60160820593468d5007cfc15e1
mark <janowski.mark.j@gmail.com>
2026-05-02 01:30:29 +0000

    2nd

--- bookbot/main.py
+++ bookbot/main.py
@@ -2,3 +2,9 @@ def get_book_text(filepath):
     with open(filepath, "r") as f:
         contents = f.read()
     return contents
+
+def main():
+    contents = get_book_text("books/frankenstein.txt")
+    print(contents)
+
+main()
