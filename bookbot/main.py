a8bbfeb7dec973faf22278bfe0fd3040eae03915
mark <janowski.mark.j@gmail.com>
2026-05-14 00:44:26 +0000

    total words#

--- bookbot/main.py
+++ bookbot/main.py
@@ -14,6 +14,7 @@ def get_book_text(filepath):
 def main():
     # Path is relative to the working directory, not this file
     contents = get_book_text("books/frankenstein.txt")
+    print(total_words(contents))
     print(total_chars(contents))
 
 main()
