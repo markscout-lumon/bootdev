613b2c1d666ce5b92e21455603e933c180ab618f
mark <janowski.mark.j@gmail.com>
2026-05-22 00:24:39 +0000

    das

--- bookbot/stats.py
+++ bookbot/stats.py
@@ -1,10 +1,12 @@
+from typing import TypedDict
+
+class CharacterCount(TypedDict):
+    char: str
+    num: int
+
 def total_words(contents):
     words = contents.split()
     return f"Found {len(words)} total words"
-
-
-
-
 
 def total_chars(contents):
     lowercase = contents.lower()
@@ -17,3 +19,14 @@ def total_chars(contents):
         else:
             counts[char] = 1
     return counts
+
+def sort_on(items):
+    # return the value used for comparison
+    return items["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for i in num_chars_dict:
+        sorted_list.append({"char": i, "num": num_chars_dict[i]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
